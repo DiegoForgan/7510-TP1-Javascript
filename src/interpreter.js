9e92c7ed704e8a3d7f19ed6b4a3690d7942e529d
DiegoForgan <dmforgan89@gmail.com>
2017-09-29 20:25:10 -0300

    Detecta si la BDD tiene formato correcto.

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,10 +1,128 @@
-var Interpreter = function () {
+function Query () {
+    var respetaFormato = false;
+    var nombre = "";
+    var valores = [];
 
-    this.parseDB = function (params, paramss, paramsss) {
-
+    this.cargarDatos = function (linea) {
+        return;
     }
 
-    this.checkQuery = function (params) {
+    this.verificarParentesis = function (linea) {
+        return (linea.search(/\(.*\)$/) != -1);
+    }
+    this.chequearValidez = function (linea) {
+        if (this.verificarParentesis(linea)) {
+            this.cargarDatos(linea);
+            respetaFormato = true;
+        } else {
+            respetaFormato = false
+        }
+        return respetaFormato;
+    }
+
+}
+
+function BaseDeDatos() {
+    var hechos = [];
+    var reglas = [];
+    var respetaFormato = false;
+
+    this.getHechos = function () {
+        return hechos;
+    }
+
+    this.getReglas = function () {
+        return reglas;
+    }
+
+    this.getValidez = function () {
+        return respetaFormato;
+    }
+
+    this.esUnaRegla = function (linea) {
+        if (linea.search(":-") != -1) {return true;}
+        return false;
+    }
+
+    this.agregarUnHecho = function (linea) {
+        hechos.push(linea);
+    }
+
+    this.agregarUnaRegla = function (linea) {
+        reglas.push(linea);
+    }
+
+    this.chequearPuntoFinal = function (cadena) {
+        return cadena.endsWith(".");
+    }
+
+    this.chequearParentesis = function (cadena){
+        if (cadena.search(/\(.*\)/) != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    this.chequearFormatoDeLaBase = function (entrada) {
+        var respetaPuntosFinales = entrada.every(this.chequearPuntoFinal);
+        var respetaParentesis = entrada.every(this.chequearParentesis);
+        respetaFormato = ((respetaPuntosFinales) && (respetaParentesis));
+    }
+
+    this.armarHechosYReglas = function (entrada) {
+        for (var i = 0; i < entrada.length; i++) {
+            var elemento = entrada[i];
+            if (this.esUnaRegla(elemento)) {
+                this.agregarUnaRegla(elemento);
+            } else {
+                this.agregarUnHecho(elemento);   
+            }   
+        } 
+    }
+
+    this.procesarEntrada = function (entrada) {
+        this.chequearFormatoDeLaBase(entrada);
+        if (respetaFormato) {
+            this.armarHechosYReglas(entrada);
+        }
+    }
+}
+
+var Interpreter = function () {
+    var bdd = new BaseDeDatos();
+    //var query = new Query();
+
+     var db = [
+        "varon(juan).",
+        "varon(pepe).",
+        "varon(hector).",
+        "varon(roberto).",
+        "varon(alejandro).",
+        "mujer(maria).",
+        "mujer(cecilia).",
+        "padre(juan, pepe).",
+        "padre(juan, pepa).",
+        "padre(hector, maria).",
+        "padre(roberto, alejandro).",
+        "padre(roberto, cecilia).",
+        "hijo(X, Y) :- varon(X), padre(Y, X).",
+        "hija(X, Y) :- mujer(X), padre(Y, X)."
+    ];
+    
+    this.quitarEspacios = function (linea) {
+        return linea.trim();
+    }
+
+    this.parseDB = function (userEntry) {
+        var sinEspacios = db.map(this.quitarEspacios);
+        bdd.procesarEntrada(db);
+    }
+
+    this.checkQuery = function (query) {
+        var consulta = new Query();
+        if(bdd.getValidez() === false){return false;}
+        if(consulta.chequearValidez(query) === false){return false;}
+        //RESOLVER LA QUERY
         return true;
     }
 
