530183d01e124588e235ed0c8d29011d642038c1
DiegoForgan <dmforgan89@gmail.com>
2017-10-07 16:37:32 -0300

    Agrego Tests de Aceptacion

--- test/interpreterAcceptanceTests.js
+++ test/interpreterAcceptanceTests.js
@@ -24,6 +24,19 @@ describe("Interpreter", function () {
         "hija(X, Y) :- mujer(X), padre(Y, X)."
     ];
 
+    var numDB = [
+        "add(zero, zero, zero).",
+	    "add(zero, one, one).",
+	    "add(zero, two, two).",
+	    "add(one, zero, one).",
+	    "add(one, one, two).",
+	    "add(one, two, zero).",
+	    "add(two, zero, two).",
+	    "add(two, one, zero).",
+	    "add(two, two, one).",
+	    "subtract(X, Y, Z) :- add(Y, Z, X)."
+    ];
+
     var interpreter = null;
 
     before(function () {
@@ -66,9 +79,7 @@ describe("Interpreter", function () {
         it('padre(mario, pepe) should be false', function () {
             assert(interpreter.checkQuery('padre(mario, pepe)') === false);
         });
-
         // TODO: Add more tests
-
     });
 
     describe('Interpreter Rules', function () {
@@ -82,10 +93,55 @@ describe("Interpreter", function () {
         it('hijo(pepe, juan) should be true', function () {
             assert(interpreter.checkQuery('hijo(pepe, juan)'));
         });
-
+        it('subtract(two, two, one) should be false', function () {
+            interpreter.parseDB(numDB);
+            assert(interpreter.checkQuery('subtract(two, two, one)') === false);
+        });
+        it('subtract(one, two, two) should be true', function () {
+            interpreter.parseDB(numDB);
+            assert(interpreter.checkQuery('subtract(one, two, two)') === true);
+        });
         // TODO: Add more tests
-
     });
+
+    describe('Bad Database Format', function () {
+
+        it('Entry without final dot should be false', function () {
+            interpreter.parseDB(["hijo(juan).","hijo(pepe)"]);
+            assert(interpreter.checkQuery('hijo(pepe)') === false);
+        });
+        it('Entry without opening parenthesis should be false', function () {
+            interpreter.parseDB(["hijojuan).","hijo(pepe)."]);
+            assert(interpreter.checkQuery('hijo(juan)') === false);
+        });
+        it('Entry without closing parenthesis should be false', function () {
+            interpreter.parseDB(["hijo(juan.","hijo(pepe)."]);
+            assert(interpreter.checkQuery('hijo(juan)') === false);
+        });
+        it('Entry without any parenthesis should be false', function () {
+            interpreter.parseDB(["hijojuan.","hijo(pepe)."]);
+            assert(interpreter.checkQuery('hijo(juan)') === false);
+        });
+        // TODO: Add more tests
+    });
+
+    describe('Bad Query Format', function () {
+
+        it('Query with final dot should be false', function () {
+            assert(interpreter.checkQuery('hijo(juan).') === false);
+        });
+        it('Query without opening parenthesis should be false', function () {
+            assert(interpreter.checkQuery('hijojuan)') === false);
+        });
+        it('Query without closing parenthesis should be false', function () {
+            assert(interpreter.checkQuery('hijo(juan') === false);
+        });
+        it('Queryy without any parenthesis should be false', function () {
+            assert(interpreter.checkQuery('hijojuan') === false);
+        });
+        // TODO: Add more tests
+    });
+    
 
 
 });
