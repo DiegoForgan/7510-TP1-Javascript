b42c7fd4b5847777ab5b1f371088b251ec7c62cd
DiegoForgan <dmforgan89@gmail.com>
2017-10-07 16:45:30 -0300

    Comento el Codigo

--- src/interpreter.js
+++ src/interpreter.js
@@ -1,23 +1,26 @@
+//A partir de una cadena de caracteres, elimina espacios a izquierda y derecha.
 function quitarEspacios(cadena) {
     return cadena.trim();
 }
-
+//Dada una cadena de caracteres correspondiente a una regla, hecho o query, devuelve el nombre de la misma.
 function obtenerNombre(linea) {
     return linea.replace(/\(.*$/,"");
 }
-
+//Dada linea, obtiene los valores que se encuentran dentro de los parentesis.
 function obtenerElementosDentroDelPrimerParentesis(linea) {
     var aux = linea.replace(/\).*$/,"");
     aux = aux.replace(/.*\(/,"");
     aux = aux.split(",");
     return (aux.map(quitarEspacios));
 }
-
+//Objeto Regla
 function Regla(linea) {
+    //Atributos privados del objeto Regla.
     var reglaCompleta = linea;
     var listaDeHechos = new Array();
     var simbolos = new Array();
 
+    //Metodos del objeto Regla
     this.getHechos = function () {
         return listaDeHechos;
     }
@@ -26,10 +29,11 @@ function Regla(linea) {
         return obtenerNombre(reglaCompleta);
     }
 
+    //Obtiene los elementos que componen la regla en su version simbolica, EJ: X, Y, Z
     this.obtenerSimbolosDeLaRegla = function (linea) {
         simbolos = obtenerElementosDentroDelPrimerParentesis(linea);
     }
-
+    //Reemplaza los simbolos obtenidos en la funcion anterior por los valores ingresados en la query.
     this.reemplazarValoresEnLaRegla = function (listaDeValores){
         var aux = reglaCompleta;
         for (var i = 0; i < simbolos.length; i++) {
@@ -37,7 +41,7 @@ function Regla(linea) {
         }
         return aux;
     }
-
+    //A partir de la regla con sus valores reemplazados, devuelve un vector con cada uno de los hechos que la componen.
     this.obtenerHechos = function (reglaConValores) {
         var hechosAUX = [];
         reglaConValores = reglaConValores.replace(/^.*:-/,"");
@@ -52,22 +56,23 @@ function Regla(linea) {
         }
         return hechosAUX.map(quitarEspacios);
     }
-
+    //Devuelve los hechos.
     this.obtenerListaDeHechosConValoresReemplazados = function (listaDeValores) {
         var reglaConValoresReemplzados = this.reemplazarValoresEnLaRegla(listaDeValores);
         return this.obtenerHechos(reglaConValoresReemplzados);
     }
 }
 
-
+//Objeto Base de Datos
 function BaseDeDatos() {
-    //Atributos de la base de datos
+    //Atributos privados de la base de datos
     var hechos = new Array();
     var hechosDisponibles = new Array();
     var reglas = new Array();
     var reglasDisponibles = new Array();
     var respetaFormato = false;
 
+    //Metodos del objeto Base de Datos
     this.getValidez = function () {
         return respetaFormato;
     }
@@ -137,7 +142,7 @@ function BaseDeDatos() {
         var respetaParentesis = entrada.every(this.chequearParentesis);
         respetaFormato = ((respetaPuntosFinales) && (respetaParentesis));
     }
-
+    //Chequea el formato de la consulta ingresada, en este caso revisa que cumpla con los parentesis.
     this.chequearFormatoDeLaConsulta = function (consulta) {
         return this.chequearParentesis(consulta);
     }
@@ -159,7 +164,7 @@ function BaseDeDatos() {
         }
         return false;
     }
-
+    // Resuelve un hecho a partir de una consulta. Devuelve TRUE si el hecho existe y FALSE caso contrario.
     this.resolverHecho = function (consulta) {
         for (var i = 0; i < hechos.length; i++) {
             if (consulta === hechos[i]){
@@ -168,13 +173,14 @@ function BaseDeDatos() {
         }
         return false;
     }
-
+    //Resuelve una regla a partir de los hechos que la componen, si todos los hechos existen devuelve TRUE, Sino FALSE.
     this.resolverRegla = function (consulta) {
         var valoresDeLaConsulta = obtenerElementosDentroDelPrimerParentesis(consulta);
         var hechosAResolver = this.obtenerHechosAPartirDeLaConsulta(obtenerNombre(consulta),obtenerElementosDentroDelPrimerParentesis(consulta));
         return (hechosAResolver.every(this.resolverHecho));
     }
 
+    // Obtiene los hechos con sus valores reemplazados a partir de los valores que figuran en la consulta.
     this.obtenerHechosAPartirDeLaConsulta = function (nombreConsulta,valoresConsulta) {
         for (var i = 0; i < reglas.length; i++) {
             if(reglas[i].getNombre() === nombreConsulta){
@@ -184,7 +190,7 @@ function BaseDeDatos() {
     }
    
 }
-//////////////////////////////////////////////////////////////////////////////////////////////////////
+
 var Interpreter = function () {
     var bdd = new BaseDeDatos();
     
